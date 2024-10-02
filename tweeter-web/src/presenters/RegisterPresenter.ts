bb30be5fa7458400836eea23046a334fa538235b
Ishmael Taleo <ishmaeltaleo@gmail.com>
2024-10-02 11:24:32 -0600

    passed off - lost half a point for not moving handleFileChanges from register component to presenter. Ready for M2B

--- tweeter-web/src/presenters/RegisterPresenter.ts
+++ tweeter-web/src/presenters/RegisterPresenter.ts
@@ -1,3 +1,4 @@
+import { Buffer } from "buffer";
 import { AuthenticatePresenter, AuthView } from "./AuthenticatePresenter";
 
 export class RegisterPresenter extends AuthenticatePresenter {
@@ -33,5 +34,44 @@ export class RegisterPresenter extends AuthenticatePresenter {
     } finally {
       this.setLoadingState(false);
     }
+  }
+
+  public handleImageFile(
+    file: File | undefined,
+    setImageUrl: (url: string) => void,
+    setImageBytes: (bytes: Uint8Array) => void,
+    setImageFileExtension: (ext: string) => void
+  ) {
+    if (file) {
+      setImageUrl(URL.createObjectURL(file));
+
+      const reader = new FileReader();
+      reader.onload = (event: ProgressEvent<FileReader>) => {
+        const imageStringBase64 = event.target?.result as string;
+        const imageStringBase64BufferContents =
+          imageStringBase64.split("base64,")[1];
+
+        const bytes: Uint8Array = Buffer.from(
+          imageStringBase64BufferContents,
+          "base64"
+        );
+
+        setImageBytes(bytes);
+      };
+      reader.readAsDataURL(file);
+
+      const fileExtension = this.getFileExtension(file);
+      if (fileExtension) {
+        setImageFileExtension(fileExtension);
+      }
+    } else {
+      setImageUrl("");
+      setImageBytes(new Uint8Array());
+    }
+  }
+
+  // Helper function for extracting the file extension
+  private getFileExtension(file: File): string | undefined {
+    return file.name.split(".").pop();
   }
 }
