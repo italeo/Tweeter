7ba339c4da3927b06ab4d03f43b884b86a3d28a7
Ishmael Taleo <ishmaeltaleo@gmail.com>
2024-10-07 17:57:11 -0600

    removed all dups from the presenter and app is working

--- tweeter-web/src/presenters/UserInfoPresenter.ts
+++ tweeter-web/src/presenters/UserInfoPresenter.ts
@@ -34,23 +34,6 @@ export class UserInfoPresenter extends Presenter<UserInfoView> {
         );
       }
     }, "determine follower status");
-    // try {
-    //   if (currentUser === displayedUser) {
-    //     this.view.setIsFollower(false);
-    //   } else {
-    //     this.view.setIsFollower(
-    //       await this.followService.getIsFollowerStatus(
-    //         authToken!,
-    //         currentUser!,
-    //         displayedUser!
-    //       )
-    //     );
-    //   }
-    // } catch (error) {
-    //   this.view.displayErrorMessage(
-    //     `Failed to determine follower status because of exception: ${error}`
-    //   );
-    // }
   }
 
   // Change to setNumberOfFollowees
@@ -60,15 +43,6 @@ export class UserInfoPresenter extends Presenter<UserInfoView> {
         await this.followService.getFolloweeCount(authToken, displayedUser)
       );
     }, "get followees count");
-    // try {
-    //   this.view.setFolloweesCount(
-    //     await this.followService.getFolloweeCount(authToken, displayedUser)
-    //   );
-    // } catch (error) {
-    //   this.view.displayErrorMessage(
-    //     `Failed to get followee because of exception: ${error}`
-    //   );
-    // }
   }
 
   public async setNumbFollowers(authToken: AuthToken, displayedUser: User) {
@@ -77,15 +51,6 @@ export class UserInfoPresenter extends Presenter<UserInfoView> {
         await this.followService.getFollowerCount(authToken, displayedUser)
       );
     }, "get followers count");
-    // try {
-    //   this.view.setFollowersCount(
-    //     await this.followService.getFollowerCount(authToken, displayedUser)
-    //   );
-    // } catch (error) {
-    //   this.view.displayErrorMessage(
-    //     `Failed to get follower because of exception: ${error}`
-    //   );
-    // }
   }
 
   public async followUserCustom(
@@ -111,30 +76,6 @@ export class UserInfoPresenter extends Presenter<UserInfoView> {
       this.view.setFollowersCount(followersCount);
       this.view.setFolloweesCount(followeesCount);
     }, "follow user");
-    // try {
-    //   // Display a message when starting the follow operation
-    //   this.view.displayInfoMessage(
-    //     `Adding ${_displayedUser!.name} to followers...`,
-    //     0
-    //   );
-
-    //   // Call the service to follow the user
-    //   let [followersCount, followeesCount] = await this.followService.follow(
-    //     _authToken!,
-    //     _displayedUser!
-    //   );
-
-    //   this.view.clearLastInfoMessage();
-
-    //   this.view.setIsFollower(true);
-    //   this.view.setFollowersCount(followersCount);
-    //   this.view.setFolloweesCount(followeesCount);
-    // } catch (error) {
-    //   // Handle any error that occurred during the follow operation
-    //   this.view.displayErrorMessage(
-    //     `Failed to follow user because of exception: ${error}`
-    //   );
-    // }
   }
 
   public async unfollowUserCustom(
@@ -155,24 +96,5 @@ export class UserInfoPresenter extends Presenter<UserInfoView> {
       this.view.setFollowersCount(followersCount);
       this.view.setFolloweesCount(followeesCount);
     }, "unfollow user");
-    // try {
-    //   this.view.displayInfoMessage(
-    //     `Removing ${_displayedUser!.name} from followers...`,
-    //     0
-    //   );
-    //   const [followersCount, followeesCount] =
-    //     await this.followService.unfollow(_authToken!, _displayedUser!);
-
-    //   this.view.clearLastInfoMessage();
-
-    //   this.view.setIsFollower(false);
-    //   this.view.setFollowersCount(followersCount);
-    //   this.view.setFolloweesCount(followeesCount);
-    // } catch (error) {
-    //   this.view.displayErrorMessage(
-    //     `Failed to unfollow user because of exception: ${error}`
-    //   );
-    //   throw error;
-    // }
   }
 }
