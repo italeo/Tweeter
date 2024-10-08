b7830eda72c135ebfd41ad9a4d043156aadc3a1b
Ishmael Taleo <ishmaeltaleo@gmail.com>
2024-10-07 18:06:55 -0600

    copleted parent View for StatusItemScroller and UseritemScroller

--- tweeter-web/src/components/postStatus/PostStatus.tsx
+++ tweeter-web/src/components/postStatus/PostStatus.tsx
@@ -32,18 +32,6 @@ const PostStatus = () => {
     await presenter.postStatus(authToken!, post, currentUser!);
   };
 
-  // ----------------- MOVE TO StatusService ------------------------------------------
-  // const postStatus = async (
-  //   authToken: AuthToken,
-  //   newStatus: Status
-  // ): Promise<void> => {
-  //   // Pause so we can see the logging out message. Remove when connected to the server
-  //   await new Promise((f) => setTimeout(f, 2000));
-
-  // TODO: Call the server to post the status
-  // };
-  // ----------------- MOVE TO StatusService ------------------------------------------
-
   const clearPost = (event: React.MouseEvent) => {
     event.preventDefault();
     setPost("");
