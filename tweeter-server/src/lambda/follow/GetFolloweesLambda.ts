aff77427e3d25eb06823ff7a399339b8fd149ce7
Ishmael Taleo <ishmaeltaleo@gmail.com>
2024-11-07 08:08:37 -0700

    removed dubs from getfollowees and get followers

--- tweeter-server/src/lambda/follow/GetFolloweesLambda.ts
+++ tweeter-server/src/lambda/follow/GetFolloweesLambda.ts
@@ -1,22 +1,18 @@
 import { PagedUserItemRequest, PagedUserItemResponse } from "tweeter-shared";
 import { FollowService } from "../../model/service/FollowService";
+import { handlePagedUserRequest } from "./FollowLambdaUtils";
 
 // Function that will be called by API Gateway when user makes a 'request' to get more followees
 export const handler = async (
   request: PagedUserItemRequest
 ): Promise<PagedUserItemResponse> => {
   const followService = new FollowService();
-  const [items, hasMore] = await followService.loadMoreFollowees(
-    request.token,
-    request.userAlias,
-    request.pageSize,
-    request.lastItem
+  return handlePagedUserRequest(() =>
+    followService.loadMoreFollowees(
+      request.token,
+      request.userAlias,
+      request.pageSize,
+      request.lastItem
+    )
   );
-
-  return {
-    success: true,
-    message: null,
-    items: items,
-    hasMore: hasMore,
-  };
 };
--- tweeter-server/src/lambda/follow/GetFollowersLambda.ts
+++ tweeter-server/src/lambda/follow/GetFollowersLambda.ts
@@ -1,20 +1,17 @@
 import { PagedUserItemRequest, PagedUserItemResponse } from "tweeter-shared";
 import { FollowService } from "../../model/service/FollowService";
+import { handlePagedUserRequest } from "./FollowLambdaUtils";
 
 export const handler = async (
   request: PagedUserItemRequest
 ): Promise<PagedUserItemResponse> => {
   const followService = new FollowService();
-  const [items, hasMore] = await followService.loadMoreFollowers(
-    request.token,
-    request.userAlias,
-    request.pageSize,
-    request.lastItem
+  return handlePagedUserRequest(() =>
+    followService.loadMoreFollowers(
+      request.token,
+      request.userAlias,
+      request.pageSize,
+      request.lastItem
+    )
   );
-  return {
-    success: true,
-    message: null,
-    items: items,
-    hasMore: hasMore,
-  };
 };
