af900c3dc88b22f886c9603fefd62b88a8798c33
Ishmael Taleo <ishmaeltaleo@gmail.com>
2024-11-07 13:18:06 -0700

    comleted StatusService test

--- tweeter-web/test/model/service/StatusService.test.ts
+++ tweeter-web/test/model/service/StatusService.test.ts
@@ -1,7 +1,26 @@
 import "isomorphic-fetch";
+import { StatusService } from "../../../src/model/service/StatusService";
+import { AuthToken } from "tweeter-shared";
 
-describe("StatusService Tests", () => {
-  it("should have a placeholder test", () => {
-    expect(true).toBe(true);
+describe("StatusService Integration Tests", () => {
+  const statusService = new StatusService();
+
+  // Returns A User's Story Pages
+  test("Retrieve a user's story", async () => {
+    const authToken: AuthToken = new AuthToken("dummyAuthToken", Date.now());
+    const userAlias = "@exampleUser";
+    const pageSize = 10;
+    const lastItem = null;
+
+    const [statuses, hasMore] = await statusService.loadMoreStoryItems(
+      authToken,
+      userAlias,
+      pageSize,
+      lastItem
+    );
+
+    expect(Array.isArray(statuses)).toBe(true);
+    expect(statuses.length).toBeLessThanOrEqual(pageSize);
+    expect(typeof hasMore).toBe("boolean");
   });
 });
